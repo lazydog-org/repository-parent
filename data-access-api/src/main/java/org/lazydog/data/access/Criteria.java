5098b30e6c15d3ed61707c7b769a5c740bc060b6
rjrjr <rjrjr@cox.net>
2010-08-20 17:15:09 +0000

    add comments to Criteria interface - rjrjr

--- data-access-api/src/main/java/org/lazydog/data/access/Criteria.java
+++ data-access-api/src/main/java/org/lazydog/data/access/Criteria.java
@@ -12,15 +12,52 @@ import org.lazydog.data.access.criterion.Criterion;
  */
 public interface Criteria<T> {
 
+    /**
+     * Add a restriction criterion.
+     *
+     * @param  criterion  the restriction criterion.
+     *
+     * @return  the criteria.
+     */
     public Criteria<T> add(Criterion criterion);
 
-    public Criteria<T> add(List<Criterion> criterions);
+    /**
+     * Add restriction criterions.
+     *
+     * @param  criterions  the restriction criterions.
+     *
+     * @return  the criteria.
+     */
+    public Criteria<T> add(List<Criterion> criterions) ;
 
+    /**
+     * Add a order criterion.
+     *
+     * @param  criterion  the order criterion.
+     *
+     * @return  the criteria.
+     */
     public Criteria<T> addOrder(Criterion criterion);
+    /**
+     * Add order criterions.
+     *
+     * @param  criterions  the order criterions.
+     *
+     * @return  the criteria.
+     */
+    public Criteria<T> addOrders(List<Criterion> criterions);
 
-    public Criteria<T> addOrders(List<Criterion> criterion);
-
+    /**
+     * Get the parameters.
+     *
+     * @return  the parameters.
+     */
     public Map<String, Object> getParameters();
 
+    /**
+     * Get the query language string.
+     *
+     * @return  the query language string.
+     */
     public String getQlString();
 }
